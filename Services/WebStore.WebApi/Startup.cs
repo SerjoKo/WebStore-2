c3879e5357b6468d0207f23731c68a4c1fcb8f0f
SerjoKo <serlo.ko@gmail.com>
2021-06-27 21:49:06 +0500

    Подключчение сервисов в приложение WebAPI

--- Services/WebStore.WebApi/Startup.cs
+++ Services/WebStore.WebApi/Startup.cs
@@ -1,15 +1,20 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using WebStore.DAL.Context.WebStore.DAL.Context;
+using WebStore.Domain.Entitys.Identity;
+using WebStore.Inerfaces.Services;
+using WebStore.Services.Data;
+using WebStore.Services.Services.InMemory;
+using WebStore.Services.Services.InSQL;
+using WebStore.Services.Servicess.InCookies;
+using WebStore.Servicess.Interfaces;
 
 namespace WebStore.WebApi
 {
@@ -25,6 +30,63 @@ namespace WebStore.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var database_name = Configuration["Database"];
+
+            switch (database_name)
+            {
+                case "WSDBSQL":
+                    services.AddDbContext<WebStoreDB>(opt =>
+                        opt.UseSqlServer(
+                            Configuration.GetConnectionString("WSDBSQL")));
+                    break;
+                case "Sqlite":
+                    services.AddDbContext<WebStoreDB>(opt =>
+                        opt.UseSqlite(
+                            Configuration.GetConnectionString("Sqlite"),
+                            o => o.MigrationsAssembly("WebStore.DAL.SqLite")));
+                    break;
+            }
+
+            services.AddTransient<WSDBInitializer>();
+
+            services.AddIdentity<User, Role>()
+                .AddEntityFrameworkStores<WebStoreDB>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(opt => {
+#if DEBUG
+                opt.Password.RequireDigit = false;
+                opt.Password.RequiredLength = 3;
+                opt.Password.RequireLowercase = false;
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequiredUniqueChars = 3;
+#endif
+                opt.User.RequireUniqueEmail = false;
+                //opt.User.AllowedUserNameCharacters = "Набор разрешенных символов";
+                opt.Lockout.AllowedForNewUsers = false;
+                opt.Lockout.MaxFailedAccessAttempts = 3;
+                opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+
+            });
+
+            services.ConfigureApplicationCookie(opt =>
+            {
+                opt.Cookie.Name = "WS_C";
+                opt.Cookie.HttpOnly = true;
+                opt.ExpireTimeSpan = TimeSpan.FromDays(5);
+
+                opt.LoginPath = "/Account/Login";
+                opt.LogoutPath = "/Account/Logout";
+                opt.AccessDeniedPath = "/Account/AccessDenied";
+
+                opt.SlidingExpiration = true;
+            });
+
+            services.AddScoped<IEmployeesData, SqlEmployeesData>();
+            services.AddScoped<ICartService, InCookiesCartService>();
+            services.AddScoped<IProductData, SqlProductData>();
+            services.AddScoped<IOrderService, SqlOrderService>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
