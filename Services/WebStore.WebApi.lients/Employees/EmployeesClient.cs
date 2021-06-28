1eb91e81c652e488bfad42d446a9e103bdc393d6
SerjoKo <serlo.ko@gmail.com>
2021-06-28 23:54:41 +0500

    Контроллер и клиент к нему, для управления сотрудниками

---
+++ Services/WebStore.WebApi.lients/Employees/EmployeesClient.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using WebStore.Domain.Entitys;
+using WebStore.Inerfaces.Services;
+using WebStore.WebApi.lients.Base;
+
+namespace WebStore.WebApi.Clients.Employees
+{
+    public class EmployeesClient : BaseClient, IEmployeesData
+    {
+        public EmployeesClient(HttpClient Client) : base(Client, "api/employees")
+        {
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return Get<IEnumerable<Employee>>(Address);
+        }
+
+        public Employee Get(int id)
+        {
+            return Get<Employee>($"{Address}/{id}");
+        }
+
+        public int Add(Employee employee)
+        {
+            var response = Post(Address, employee);
+            var id = response.Content.ReadFromJsonAsync<int>().Result;
+            return id;
+        }
+
+        public bool Delete(int id)
+        {
+            var result = Delete($"{Address}/{id}").IsSuccessStatusCode;
+            return result;
+        }
+
+        public void Update(Employee employee)
+        {
+            Put(Address, employee);
+        }
+    }
+}
