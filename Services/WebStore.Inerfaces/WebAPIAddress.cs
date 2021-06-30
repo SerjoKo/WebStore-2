72ef0565acf3503a37907b7fbe4a061e6e23c1e3
SerjoKo <serlo.ko@gmail.com>
2021-06-30 19:11:20 +0500

    Константы адресов конечных точек

---
+++ Services/WebStore.Inerfaces/WebAPIAddress.cs
@@ -0,0 +1,8 @@
+﻿namespace WebStore.Inerfaces
+{
+    public static class WebAPIAddress
+    {
+        public const string Employees = "api/employees";
+        public const string Products = "api/produtcs";
+    }
+}
