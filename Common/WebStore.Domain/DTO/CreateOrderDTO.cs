74d15fb7482e0a1ceb815ee320db0aac4e55dc45
SerjoKo <serlo.ko@gmail.com>
2021-07-01 00:58:50 +0500

    Набор DTO моделей

---
+++ Common/WebStore.Domain/DTO/CreateOrderDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using WebStore.Domain.ViewModels;
+
+namespace WebStore.Domain.DTO
+{
+    public static partial class OrderMapper
+    {
+        public class CreateOrderDTO
+        {
+            /// <summary>Модель заказа</summary>
+            public OrderViewModel Order { get; set; }
+
+            /// <summary>Пункты заказа</summary>
+            public IEnumerable<OrderItemDTO> Items { get; set; }
+        }
+    }
+}
---
+++ Common/WebStore.Domain/DTO/OrderDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using WebStore.Domain.Entitys;
+using WebStore.Domain.Entitys.Orders;
+
+namespace WebStore.Domain.DTO
+{
+    public class OrderDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public IEnumerable<OrderItemDTO> Items { get; set; }
+    }
+
+    
+}
