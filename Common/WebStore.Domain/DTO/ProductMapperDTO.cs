81f976ab16a046ad36e38ef87b3d693e145a8471
SerjoKo <serlo.ko@gmail.com>
2021-06-30 21:59:49 +0500

    DTO  модели товаров брендов и секций

---
+++ Common/WebStore.Domain/DTO/ProductMapperDTO.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WebStore.Domain.Entitys;
+
+namespace WebStore.Domain.DTO
+{
+    public static class ProductMapperDTO
+    {
+        public static ProductDTO ToDTO(this Product Product) => Product is null
+            ? null
+            : new ProductDTO
+            {
+                Id = Product.Id,
+                Name = Product.Name,
+                Order = Product.Order,
+                Price = Product.Price,
+                ImgUrl = Product.ImgUrl,
+                Brand = Product.Brand.ToDTO(),
+                Section = Product.Section.ToDTO(),
+            };
+
+        public static Product FromDTO(this ProductDTO Product) => Product is null
+            ? null
+            : new Product
+            {
+                Id = Product.Id,
+                Name = Product.Name,
+                Order = Product.Order,
+                Price = Product.Price,
+                ImgUrl = Product.ImgUrl,
+                Brand = Product.Brand.FromDTO(),
+                Section = Product.Section.FromDTO(),
+            };
+
+        public static IEnumerable<ProductDTO> ToDTO(this IEnumerable<Product> Products) => Products.Select(ToDTO);
+        public static IEnumerable<Product> FromDTO(this IEnumerable<ProductDTO> Products) => Products.Select(FromDTO);
+    }
+}
